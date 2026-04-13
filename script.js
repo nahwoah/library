3aa566c45b984127369bbd0b34dea5e901861da4
nahwoah <119922465+nahwoah@users.noreply.github.com>
2026-04-13 14:53:31 +0300

    finished up to 3. load the data into the table

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const myLibrary = []
+const exarray = ['1', '2']
+const table = document.querySelector("table")
 
 function Book ( title, author, pages, read){
     this.id = crypto.randomUUID();
@@ -9,10 +11,29 @@ function Book ( title, author, pages, read){
 }
 
 function addToBookLibrary (){
-    const book = new Book("Hobbit", "tolkien", 200, false);
-    myLibrary.push(book);
+    const book1 = new Book("Hobbit", "tolkien", 200, false);
+
+    const book2 = new Book("lotr", "tolkien", 300, false);
     
+    const book3 = new Book("Silm", "tolkien", 400, false);
+    myLibrary.push(book1, book2, book3);
 }
 
 addToBookLibrary();
 console.log(myLibrary);
+
+// const tableRows = document.createElement('tr');
+// const tableData = document.createElement('td');
+
+// tableRows.textContent = myLibrary;
+
+myLibrary.forEach(item => {
+    const tableRow = document.createElement('tr');
+
+    Object.keys(item).forEach(key => {
+        const tbData = document.createElement('td');
+        tbData.textContent = item[key];
+        tableRow.appendChild(tbData);
+    })
+    table.appendChild(tableRow);
+})
