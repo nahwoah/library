df80b097669d8cebcd9cb6e4dcac68b2615a6a5e
nah <nah@MacBook-Pro-Work.local>
2026-04-16 15:49:07 +0300

    no big changes

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const myLibrary = []
-const exarray = ['1', '2']
 const table = document.querySelector("table");
 const tbody = document.querySelector("tbody");
 const newbook = document.getElementById('new-book');
@@ -16,9 +15,14 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.delete = function deleteRow(r) {
-        let i = r.parentNode.parentNode.rowIndex;
-        document.getElementById("myTable").deleteRow(i);
+    this.delete = function deleteRow() {
+        // const deleteBtn = document.createElement('button');
+        // deleteBtn.textContent = 'Delete';
+        // tableRow.appendChild(deleteBtn);
+        // let i = this.parentNode.parentNode.rowIndex;
+        document.querySelector()
+        deleteBtn.onclick = 
+        document.querySelector("tbody").deleteRow(this);
     }
 }
 
@@ -33,21 +37,31 @@ function addToBookLibrary() {
 }
 
 function clearTable() {
-    while (tbody.rows.length > 0){
+    while (tbody.rows.length > 0) {
         tbody.deleteRow(0);
     }
 }
 
 function addToTable() {
-    // clearTable();
-    tbody.innerHTML = "";
+    clearTable();
     myLibrary.forEach(item => {
         const tableRow = document.createElement('tr');
 
-        Object.keys(item).forEach(key => {
-            const tbData = document.createElement('td');
-            tbData.textContent = item[key];
-            tableRow.appendChild(tbData);
+        Object.keys(item).forEach((key, idx) => {
+            if (idx === Book.length +1) {
+                console.log(Book.length, idx);
+                const deleteBtn = document.createElement('button');
+                deleteBtn.textContent = 'Delete';
+                tableRow.appendChild(deleteBtn);
+            }else{
+                const tbData = document.createElement('td');
+                tbData.textContent = item[key];
+                tableRow.appendChild(tbData);
+            }
+
+            // const tbData = document.createElement('td');
+            // tbData.textContent = item[key];
+            // tableRow.appendChild(tbData);
         })
         tbody.appendChild(tableRow);
     })
@@ -68,7 +82,7 @@ submit.addEventListener("click", function (e) {
 
 })
 
-function deleteRow(r) {
-    let i = r.parentNode.parentNode.rowIndex;
-    document.getElementById("myTable").deleteRow(i);
-}
+// function deleteRow(r) {
+//     let i = r.parentNode.parentNode.rowIndex;
+//     document.getElementById("myTable").deleteRow(i);
+// }
